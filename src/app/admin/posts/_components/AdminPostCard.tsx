89e99a042dcbf95bcd8fe9d47886c6346963ac6e
Kazuya-Sakashita <kz0508@gmail.com>
2025-02-19 18:27:37 +0900

    画面遷移をLinkに変更

--- src/app/admin/posts/_components/AdminPostCard.tsx
+++ src/app/admin/posts/_components/AdminPostCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { memo, useMemo } from "react";
-import { useRouter } from "next/navigation"; // 追加
 import { Post } from "@/app/_types/post";
 import { formatDate } from "@/app/lib/utils/dateUtils"; // 日付フォーマット関数をインポート
+import Link from "next/link";
 
 type Props = {
   post: Post;
@@ -11,7 +11,6 @@ type Props = {
 };
 
 const AdminPostCard: React.FC<Props> = ({ post }) => {
-  const router = useRouter(); // useRouter フックを追加
   console.log("Rendering PostCard", post.id);
   console.log("カテゴリデータ:", post.categories);
 
@@ -22,20 +21,15 @@ const AdminPostCard: React.FC<Props> = ({ post }) => {
 
   console.log("postデータ:", post);
 
-  // ポスト全体をクリックしたときに詳細ページへ遷移
-  const handlePostClick = () => {
-    router.push(`/admin/posts/${post.id}`); // 詳細ページに遷移
-  };
-
   return (
-    <div className="border-b-2 pb-4 cursor-pointer" onClick={handlePostClick}>
-      {" "}
-      {/* ここでクリックイベントを設定 */}
-      <h2 className="text-2xl mt-4 mb-2 truncate text-left">{post.title}</h2>
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
-        <p className="text-xs text-gray-500">{formattedDate}</p>
+    <Link href={`/admin/posts/${post.id}`} passHref>
+      <div className="border-b-2 pb-4 cursor-pointer">
+        <h2 className="text-2xl mt-4 mb-2 truncate text-left">{post.title}</h2>
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+          <p className="text-xs text-gray-500">{formattedDate}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
